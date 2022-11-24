05c68d9c3ab278ec34f602f138bebed0424eff5b
Fatih20 <fnri39@protonmail.com>
2022-11-24 10:20:14 +0700

    Attempt to implement accessing an element by square bracket.

--- test/stest_bench.js
+++ test/stest_bench.js
@@ -1,5 +1,7 @@
-function point_path(x, y) {
-  // bruh = point_and_path[(x, y)];
-  point_and_path[5] = 1;
-  return written_result;
-}
+let point_and_path = 1;
+
+// function point_path(x, y) {
+//   // bruh = point_and_path[(x, y)];
+//   point_and_path[5] = 1;
+//   return written_result;
+// }
