e51775a6368a9f6b3d9607735694f75e89e0cac4
Fatih20 <fnri39@protonmail.com>
2022-11-25 13:05:22 +0700

    Add proper test case.

--- test/function/tfunc9.js
+++ test/function/tfunc9.js
@@ -1,29 +1,17 @@
-// function func7() {
-//   while (true) {
-//     while (true) {
-//       return 0;
-//     }
-//   }
-// }
-
-// function func7() {
-//   if (bruh) {
-//     if (bruh) {
-//       return 7;
-//     }
-//   }
-// }
-
-function func7() {
-  for (i = 0; i < 7; i++) {
-    while (bruh) {
-      if (bruh) {
-        return 7;
-      }
-      while (bruh) {
-        return bruh;
-      }
-      return 8;
+function tiit() {
+  if (x <= 0) {
+    x = 2 + 5;
+    x = 25;
+    return 0;
+  } else if ((x + 4) >>> 3 == 1) {
+    if (censored) {
+      return 3;
+    } else {
+      return 2;
     }
+  } else if (x == 32) {
+    return 2 + x + (x + 6) ? "t" : x + 9 ? "b" : "sss";
+  } else {
+    return "another censored";
   }
 }
