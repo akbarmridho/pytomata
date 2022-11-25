119e72035044618c2e9ce7038ed5dac50b2b9307
Fatih20 <fnri39@protonmail.com>
2022-11-25 13:03:49 +0700

    Nested return working.

---
+++ test/function/tfunc9.js
@@ -0,0 +1,29 @@
+// function func7() {
+//   while (true) {
+//     while (true) {
+//       return 0;
+//     }
+//   }
+// }
+
+// function func7() {
+//   if (bruh) {
+//     if (bruh) {
+//       return 7;
+//     }
+//   }
+// }
+
+function func7() {
+  for (i = 0; i < 7; i++) {
+    while (bruh) {
+      if (bruh) {
+        return 7;
+      }
+      while (bruh) {
+        return bruh;
+      }
+      return 8;
+    }
+  }
+}
--- test/stest_bench.js
+++ test/stest_bench.js
@@ -2,24 +2,20 @@ function tiit() {
   if (x <= 0) {
     x = 2 + 5;
     x = 25;
-    // return 0;
+    return 0;
   } else if ((x + 4) >>> 3 == 1) {
     if (censored) {
-      //   x = 5;
-      //   return 3;
+      return 3;
     } else {
-      //   x = 5;
-      //   return 2;
+      return 2;
     }
   } else if (x == 32) {
-    // x = 5;
-    // return 2 + x + (x + 6) ? "t" : x + 9 ? "b" : "sss";
+    return 2 + x + (x + 6) ? "t" : x + 9 ? "b" : "sss";
   } else {
-    // x = 5;
-    // return "another censored";
+    return "another censored";
   }
 }
 
 let bla = 2 + x + (x + 6) ? "t" : x + 9 ? "b" : "sss";
 
-let sss = ++x + --y;
+let sss = ++x;
